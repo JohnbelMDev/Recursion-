0b9b9d94bae4e803dc00f117e8aa55a4ce2533f6
Johnbel Mahautiere <47404011+johnbel458@users.noreply.github.com>
2020-05-29 14:28:40 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+ myfunction(5);
+
+
+ function myfunction(number)
+{
+    if(number >0 )
+    {
+    console.log("I'm a recursive function and I'm showing the number of times I'm called before the function = " + number);
+     myfunction(number-1);
+   }
+     // console.log("Number ")
+     console.log("I'm a recursive function and I'm showing the number after it's being passed = " + number);
+
+
+
+}
